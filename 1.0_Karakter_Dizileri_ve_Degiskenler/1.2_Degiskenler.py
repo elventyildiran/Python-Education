25d1f650699730e2f5bcc5c475da8c59ec037812
elventyildiran <bilgi.yildiran@outlook.com>
2023-10-12 00:49:33 +0300

    Örnekler çoğaltıldı.

--- 1.0_Karakter_Dizileri_ve_Degiskenler/1.2_Degiskenler.py
+++ 1.0_Karakter_Dizileri_ve_Degiskenler/1.2_Degiskenler.py
@@ -66,3 +66,93 @@ print(type(len(isim) + len(sifre))) # <class 'int'>
 k_adi_uzunlugu = len(isim)
 print(k_adi_uzunlugu) # 16
 
+#* Örnek 1 ;
+
+sayi1 = 10
+sayi2 = 15
+topla = sayi1 + sayi2
+print(topla) # 25
+
+
+#! --- Değişken adı belirleme kuralları ---
+# Öncelikle şurdan başlayalım ; Aşağıda görmüş  olduğunuz isimler python programlama dilinde özel anlam ifade eden kelimelerdir. Bu yüzden bunları kullanamazsınız...Aşağıda yazan bütün anahtar kelimeleri ilerleyen süreçlerde göreceğiz.
+ 
+#* ['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+
+# Değişken adları bir sayı ile başlayamaz > 3_kilo_elma = "5 TL"
+# Değişken adları aritmetik işleçlerle başlayamaz >  +deger = 4576
+# Değişken adları ya bir alfabe harfiyle ya da _ işaretiyle başlamalıdır > _deger = 4576 veya > deger = 4576
+
+# Değişken adları içinde Türkçe karakterler kullanabilirsiniz. Ancak ileride beklenmedik uyum sorunları çıkması ihtimaline karşı değişken adlarında Türkçe karakter kullanmaktan kaçınmak isteyebilirsiniz.
+# (Naçizane tavsiyem : Farklı bir programlama dili daha öğrenmek istiyorsanız ilerleyen zamanlarda bu yüzden değişkenlerde asla türkçe karakter kullanmamayı alışkanlık haline getirin.)
+
+#, Yukarıda kaç adet anahtar kelime var ? Hemen bakalım ;
+import keyword
+print(len(keyword.kwlist)) # 35
+
+#*Örneği yazıyla süsleyelim ;
+print("Yukarıda toplam :", len(keyword.kwlist), "anahtar kelime mevcut.") # 35
+
+
+#! ---- Anahtar kelimeleri değişken adı olarak kullanırsak ne olur ? ---
+
+#* Örnek 1 ;
+
+type = 5
+print(type) # 5
+
+# Buraya kadar bir sorun yok "type" anahtar kelimesinin bir verinin türünü öğrenmek amaçlı kullanıldığını biliyoruz. Ancak yukarıda değişken olarak tanımladık ve şuanlık bize istediğimiz sonucu sorunsuz bir şekilde verdi. Peki ileride bir verinin tipini öğrenme ihtiyacı duyduğumuz zamanda sorunsuz çalışacak mı? Hemen kontrol edelim ;
+
+isim = "Elvent"
+#!print(type(isim))
+
+#* Exception has occurred: TypeError
+#* 'int' object is not callable
+#*   File "C:\Python-Education", line 107, in <module>
+#*     print(type(isim))
+#*           ^^^^^^^^^^
+#* TypeError: 'int' object is not callable
+
+# o da ne ! işte bir hatayla karşılaştık. Ancak bu durumdan kurtulmak için 
+#* del type
+# kullanıp type tipini silebiliriz. Bu yüzden degiskenlerde asla anahtar kelimeler kullanmamaya özen gösteriyoruz.
+
+# ! ---- Uygulama Örnekleri ----
+
+#* Örnek 1 ;
+#?- 1. Cumartesi - Pazar günleri çalışmıyoruz.
+#?- 2. Dolayısıyla ayda 22 gün çalışıyoruz.
+#?- 3. Evden işe gitmek için kullandığımız vasıtanın ücreti 1.5 TL
+#?- 4. İşten eve dönmek için kullandığımız vasıtanın ücreti 1.4 TL
+#* Aylık yol masrafımızı hesaplayabilmek için gidiş ve dönüş ücretlerini toplayıp, bunları çalıştığımız gün sayısıyla çarpmamız yeterli olacaktır. Elimizdeki bu bilgiere göre aylık yol masrafımızı hesaplamak için şöyle bir formül üretebiliriz.
+
+calisma_gunu = 22
+gidis_ucreti = 1.5
+donus_ucreti = 1.4
+masraf = (gidis_ucreti + donus_ucreti) * calisma_gunu
+print("Aylık yol masrafım : ", masraf) #63.8
+# Demek ki aylık yol masrafımız 63.8 TL imiş.
+
+# Bu arada, yukarıdaki örnekte bir şey dikkatinizi çekmiş olmalı. Aritmetik işlemi yaparken bazı sayıları parantez içine aldık. Python’da aritmetik işlemler yapılırken alıştığımız matematik kuralları geçerlidir. Yani mesela aynı anda bölme, çıkarma, toplama ve çarpma işlemleri yapılacaksa işlem öncelik sırası önce bölme ve çarpma, sonra toplama ve çıkarma şeklinde olacaktır. Elbette siz parantezler yardımıyla bu işlem sırasını değiştirebilirsiniz.
+
+# Bu anlattıklarımıza göre, eğer yukarıda yol masrafını hesaplayan programda parantezleri kullanmazsak, işlem öncelik kuralları gereğince Python önce 22 ile 1.5’i çarpıp, çıkan sonucu 1.4 ile toplayacağı için elde ettiğimiz sonuç yanlış çıkacaktır. Bizim burada doğru sonuç alabilmemiz için önce 1.5 ile 1.4’ü toplamamız, çıkan sonucu da 22 ile çarpmamız gerekiyor. Bu sıralamayı da parantezler yardımıyla elde ediyoruz.
+
+#* Örnek 2;
+
+#? 1 yılda kaç gün çalıştığımızı hesaplayalım. 
+
+aylik_calisma_gunu = 22
+toplam_ay = 12
+toplam_calisma_gunu = aylik_calisma_gunu * toplam_ay
+print("Bir yılda toplam", toplam_calisma_gunu, "gün çalışıyoruz.") #264
+
+#* Örnek 3 ;
+#? 1 Yıldaki toplam yol masrafımızı hesaplayalım;
+
+aylik_calisma_gunu = 22 #* 1 ayda toplam 22 gün çalışıyoruz.
+toplam_ay = 12 #* 1 yılda toplam 12 ay var.
+gidis_ucreti = 1.5 #* işe giderken 1.5 tl ödüyoruz
+donus_ucreti = 1.4 #* dönerken 1.4 tl ödüyoruz
+gunluk_masraf = gidis_ucreti + donus_ucreti #* gidis ve donus ucretini topluyoruz
+toplam_masraf = aylik_calisma_gunu * toplam_ay * gunluk_masraf #* calisma gunu, toplam ay ve günlük masrafı çarpıyoruz.
+print(toplam_masraf) # 765.6
